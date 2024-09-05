b30847a455736f109d4fcd606d223ce195abdf61
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-09-05 21:14:35 +0300

    starting with the welcome screen

---
+++ lib/Screens/welcom_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class WelcomScreen extends StatelessWidget {
+  const WelcomScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff6F43F5),
+      body: Column(
+        children: [
+          Image.asset("assets/logo.png"),
+        SizedBox(
+        
+        )
+        ],
+      ),
+    );
+  }
+}
