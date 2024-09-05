e7496ef51a932c679968cf39d784776499ca2c80
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-05 21:19:11 +0300

    UPD SignUpScreen

--- tickets_app/lib/screens/sign_up_screen.dart
+++ tickets_app/lib/screens/sign_up_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tickets_app/extensions/get_size.dart';
 import 'package:tickets_app/extensions/string_ext.dart';
 import 'package:tickets_app/widget/my_t_field.dart';
 import '../extensions/color_ext.dart';
@@ -9,13 +10,56 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: C.bg,
       body: SafeArea(
-        child: Column(
-          children: [
-            MyTextField(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text('Sign Up').styled(color: C.accent),
+              SizedBox(height: context.getScreenSize(multiplyWidth: 0.4)),
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      const CircleAvatar(
+                        radius: 60,
+                        backgroundColor: C.accent,
+                        child: Icon(
+                          FontAwesomeIcons.user,
+                          color: C.bg,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 5,
+                        right: 5,
+                        child: CircleAvatar(
+                            radius: 55,
+                            child: IconButton(
+                                onPressed: () {}, icon: const Icon(Icons.add))),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: context.getScreenSize(multiplyWidth: 0.05)),
+                  const MyTextField(
+                    prefixIcon: Icon(FontAwesomeIcons.user),
+                    hintText: 'Username',
+                  ),
+                  SizedBox(height: context.getScreenSize(multiplyWidth: 0.05)),
+                  const MyTextField(
+                    prefixIcon: Icon(FontAwesomeIcons.envelope),
+                    hintText: 'Email',
+                  ),
+                  SizedBox(height: context.getScreenSize(multiplyWidth: 0.05)),
+                  const MyTextField(
+                    prefixIcon: Icon(FontAwesomeIcons.phone),
+                    hintText: 'Phone Number',
+                  ),
+                  SizedBox(height: context.getScreenSize(multiplyWidth: 0.05)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
