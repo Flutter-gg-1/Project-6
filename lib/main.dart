5be256e855213be099a36364612a9753827a3baa
anas <anas.aljuhani.dev@gmail.com>
2024-09-06 19:05:26 +0300

    UI SignUp Screen is Done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:clothes_app/src/login_screen.dart';
+import 'package:clothes_app/src/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +11,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LoginScreen());
+    return const MaterialApp(home: RegisterScreen());
   }
 }
