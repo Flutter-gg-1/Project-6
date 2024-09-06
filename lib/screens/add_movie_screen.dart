7abe8e34540a763a1a51c5c6cd5b512bebde6b51
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-09-06 22:50:20 +0300

    done with the add movie style

--- lib/screens/add_movie_screen.dart
+++ lib/screens/add_movie_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:project6/bloc_movie/movie_bloc.dart';
 import 'package:project6/extensions/nav.dart';
@@ -16,52 +17,60 @@ class AddMovieScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = context.read<MovieBloc>();
     return Scaffold(
+      backgroundColor: const Color(0xff15141F),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: const Color(0xff15141F),
-        appBar: AppBar(
-          forceMaterialTransparency: true,
-          automaticallyImplyLeading: true,
+        title: Text(
+          "Add Movie",
+          style: GoogleFonts.mulish(
+              color: Colors.white, fontSize: 30, fontWeight: FontWeight.w700),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(26.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                AddFieldWidget(
-                    label: "Name",
-                    hint: "Enter movie name",
-                    controller: bloc.nameController),
-                const SizedBox(height: 40),
-                CatgoriesWidget(
-                  radioButtonValue: (value) {
-                    bloc.catValue = value;
-                    print(bloc.catValue);
-                  },
-                ),
-                const SizedBox(height: 40),
-                AddFieldWidget(
-                    label: "Date",
-                    hint: "Add movie date",
-                    controller: bloc.dateController),
-                const SizedBox(height: 40),
-                ImageFieldWidget(
-                  onSelect: () async {
-                    final selectedImage = await ImagePicker()
-                        .pickImage(source: ImageSource.gallery);
-                    bloc.image = File(selectedImage!.path);
-                  },
-                ),
-                const SizedBox(height: 40),
-                ButtonWidget(
-                    title: 'Add',
-                    onPressed: () {
-                      bloc.add(AddMovieEvent());
+        centerTitle: true,
+        forceMaterialTransparency: true,
+        automaticallyImplyLeading: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(26.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              AddFieldWidget(
+                  label: "Name",
+                  hint: "Enter movie name",
+                  controller: bloc.nameController),
+              const SizedBox(height: 40),
+              CatgoriesWidget(
+                radioButtonValue: (value) {
+                  bloc.catValue = value;
+                  print(bloc.catValue);
+                },
+              ),
+              const SizedBox(height: 40),
+              AddFieldWidget(
+                  label: "Date",
+                  hint: "Add movie date",
+                  controller: bloc.dateController),
+              const SizedBox(height: 40),
+              ImageFieldWidget(
+                onSelect: () async {
+                  final selectedImage = await ImagePicker()
+                      .pickImage(source: ImageSource.gallery);
+                  bloc.image = File(selectedImage!.path);
+                },
+              ),
+              const SizedBox(height: 40),
+              ButtonWidget(
+                  title: 'Add',
+                  onPressed: () {
+                    bloc.add(AddMovieEvent());
 
-                      context.pop();
-                    }),
-              ],
-            ),
+                    context.pop();
+                  }),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
